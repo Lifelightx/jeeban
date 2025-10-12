59a08d6140d122902cf8857e13d97574cedd0b91
Jeebanjyoti Mallik <jeebanjyotimallik01@gmail.com>
2025-10-12 15:09:46 +0530

    update about

--- PORTFOLIO-2026/src/data/about.js
+++ PORTFOLIO-2026/src/data/about.js
@@ -1,12 +1,16 @@
-export default  `import React from 'react';
+export default `import React from 'react';
 
 const About = () => {
   return (
     <div className="about-section">
       <h1>About Me</h1>
-      <p>Passionate developer with X years of experience...</p>
+      <p>
+        I’m Jeeban — a passionate Fullstack developer who loves crafting
+        interactive, elegant, and high-performance web experiences.
+      </p>
     </div>
   );
 };
 
-export default About;`
+export default About;
+`;
