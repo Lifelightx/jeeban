139b7dc44f77e304d8541c0f634d6119b30cf3ac
Jeebanjyoti Mallik <jeebanjyotimallik01@gmail.com>
2025-10-12 14:56:26 +0530

    update url

--- PORTFOLIO-2026/src/components/Editor/MessageEditor.jsx
+++ PORTFOLIO-2026/src/components/Editor/MessageEditor.jsx
@@ -8,7 +8,7 @@ const MessageEditor = ({ initialContent = '', onChange }) => {
 
   useEffect(() => {
     // Connect to WebSocket server
-    const ws = new WebSocket('ws://localhost:3001');
+    const ws = new WebSocket('https://jeeban-portfolio-backend.onrender.com');
     wsRef.current = ws;
 
     ws.onopen = () => {
